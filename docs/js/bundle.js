47b54d80e15733d5a84092f83ae51326b87f7e7a
pixelaria <info@pixelaria.ru>
2017-11-26 11:59:09 +0300

    Desktop filter, checkboxes, dropdown, basic range picker

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,2 +1,2 @@
 
-$(function(){console.log('init');$('.search__toggler').click(function(e){console.log('search toggler');var target=$(this).data('target');$('#'+target).toggleClass('search--active');return false;});});
+$(function(){console.log('init');$('.search__toggler').click(function(e){console.log('search toggler');var target=$(this).data('target');$('#'+target).toggleClass('search--active');return false;});$('.accordeon__preview').click(function(e){$(this).closest('.accordeon').toggleClass('accordeon--active');});$('.checkbox__label').click(function(e){$(this).closest('.accordeon').toggleClass('accordeon--active');});$('.im--phone').mask('+7 (000) 000-00-00');$('.input-group__input').change(function(e){$(this).toggleClass('used',$(this).val()!='');});1});
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,4 +7,21 @@ $(function (){
     $('#'+target).toggleClass('search--active');
     return false;
   });
+
+  $('.accordeon__preview').click(function(e){
+    $(this).closest('.accordeon').toggleClass('accordeon--active');
+  });
+
+  $('.checkbox__label').click(function(e){
+    $(this).closest('.accordeon').toggleClass('accordeon--active');
+  });
+
+
+  $('.im--phone').mask('+7 (000) 000-00-00');
+
+
+  $('.input-group__input').change(function(e){
+    $(this).toggleClass('used',$(this).val()!='');
+  });1
+
 });
