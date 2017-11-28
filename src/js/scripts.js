5a6a54f50f32b3de86bb7f3190d5a65b833c083f
pixelaria <info@pixelaria.ru>
2017-11-29 01:15:34 +0300

    Added: baron.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -47,4 +47,26 @@ $(function (){
     $('.tabs__body[data-name="'+target+'"]').addClass('tabs__body--active');
   });
 
+
+  if ($('.baron').length) {
+    baron({
+        root: '.baron',
+        scroller: '.baron__scroller',
+        bar: '.baron__bar',
+        scrollingCls: '_scrolling',
+        draggingCls: '_dragging'
+    }).fix({
+        elements: '.header__title',
+        outside: 'header__title_state_fixed',
+        before: 'header__title_position_top',
+        after: 'header__title_position_bottom',
+        clickable: true
+    }).controls({
+        // Element to be used as interactive track. Note: it could be different from 'track' param of baron.
+        track: '.baron__track',
+        forward: '.baron__down',
+        backward: '.baron__up'
+    });
+  }
+
 });
