54bbc4b36d84b666148442e760d92b25589874b0
Pixelaria <info@pixelaria.ru>
2017-12-01 18:40:43 +0300

    Custom synchronized scrollbars

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -95,30 +95,15 @@ $(function (){
     });
 
 
-
-
     
     $('.clipper-1').css('max-height',$('.clipper-1').height()-25);
     $('.clipper-2').css('max-height',$('.clipper-2').height()-25);
-    /*baron({
-        root: '.baron',
-        scroller: '.baron__scroller',
-        bar: '.baron__bar',
-        scrollingCls: '_scrolling',
-        draggingCls: '_dragging',
-        direction: 'h'
-    }).fix({
-        elements: '.header__title',
-        outside: 'header__title_state_fixed',
-        before: 'header__title_position_top',
-        after: 'header__title_position_bottom',
-        clickable: true
-    }).controls({
-        // Element to be used as interactive track. Note: it could be different from 'track' param of baron.
-        track: '.baron__track',
-        forward: '.baron__down',
-        backward: '.baron__up'
-    });*/
-  }
 
+    $(".scroller-1").scroll(function() {
+      $(".scroller-2").scrollLeft($(".scroller-1").scrollLeft());
+    });
+    $(".scroller-2").scroll(function() {
+      $(".scroller-1").scrollLeft($(".scroller-2").scrollLeft());
+    });
+  }
 });
