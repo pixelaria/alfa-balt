5ce039fa31b048b12f480b62fbf4f78f1053c5c4
Pixelaria <info@pixelaria.ru>
2017-12-08 20:13:29 +0300

    Serivces page base structure

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -87,7 +87,7 @@ $(function (){
           }
         },
         {
-          breakpoint: 576,
+          breakpoint: 768,
           settings: {slidesToShow: 1, slidesToScroll: 1,dots:true,}
         },
 
